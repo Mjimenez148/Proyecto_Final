c0cd6ebf9a93c650b22fa9acafda2a53d9861387
Mjimenez148 <mjimenezh5@miumg.edu.gt>
2024-05-21 14:47:42 -0600

    Update proyecto_final.py
    se crearon los def para las funciones de horas extra y el reporte aun no contienen codigo

--- proyecto_final.py
+++ proyecto_final.py
@@ -5,9 +5,22 @@ def VentanaPrincipal(page:ft.Page):
     page.title = "Ventana Principal"
     page.theme_mode = ft.ThemeMode.LIGHT
     page.update()
-    titulo= ft.Text("Registro de Personal")
+    titulo= ft.TextField("Registro de Personal")
     codigo = ft.TextField(label = 'Codigo empleado')
     nombre =ft.TextField(label='Nombre')
     costo_hr_extra = ft.TextField(label = ' Costo por Hora extra')
+    btHORAS = ft.TextButton("Ingreso Hrs Extra", on_click=lambda _:clickHrsEx(page))
+    btReporte = ft.TextButton("Reporte de horas por empleado", on_click=lambda _:clickReporte(page))
+    filatitulo = ft.row()
+    filabotones = ft.row()
+    filatitulo.controls = [titulo]
+    filabotones.controls = [btHORAS,btReporte]
+    page.add(filabotones, filatitulo)
 
+    def clickHrsEx(page:ft.Page):
+      pass
 
+    def clickReporte(page: ft.page):
+       pass
+
+ft.app(target = VentanaPrincipal)
