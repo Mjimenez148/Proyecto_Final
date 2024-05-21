0e8a9cf5ba35f7691dd4cae221db20bc2985327a
Mjimenez148 <mjimenezh5@miumg.edu.gt>
2024-05-21 14:36:25 -0600

    Update proyecto_final.py
    se esta trabajando en la ventana principal y sus botones

--- proyecto_final.py
+++ proyecto_final.py
@@ -1,2 +1,13 @@
-import flet 
-print("prubea de commit")
+import flet as ft
+datos_empleados = {}
+
+def VentanaPrincipal(page:ft.Page):
+    page.title = "Ventana Principal"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.update()
+    titulo= ft.Text("Registro de Personal")
+    codigo = ft.TextField(label = 'Codigo empleado')
+    nombre =ft.TextField(label='Nombre')
+    costo_hr_extra = ft.TextField(label = ' Costo por Hora extra')
+
+
