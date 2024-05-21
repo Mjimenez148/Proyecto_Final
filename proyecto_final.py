7626a876230981bab3a024baf2319e504db7dfdd
Mjimenez148 <mjimenezh5@miumg.edu.gt>
2024-05-21 15:07:09 -0600

    Update proyecto_final.py
    la ventana principal con los botones para ingresar un empleado, colocar las horas y el boton de reporte ya estan creados, ahora trabajare en el boton para poder ingresar los datos de un empleado y que se guarden en un JSON

--- proyecto_final.py
+++ proyecto_final.py
@@ -5,22 +5,25 @@ def VentanaPrincipal(page:ft.Page):
     page.title = "Ventana Principal"
     page.theme_mode = ft.ThemeMode.LIGHT
     page.update()
-    titulo= ft.TextField("Registro de Personal")
-    codigo = ft.TextField(label = 'Codigo empleado')
+    titulo= ft.Text("Registro de Personal")
+    codigo = ft.TextField(label = 'Codigo empleado', autofocus=True)
     nombre =ft.TextField(label='Nombre')
     costo_hr_extra = ft.TextField(label = ' Costo por Hora extra')
+    codigo_consulta = ft.TextField(label="Codigo de empleado")
     btHORAS = ft.TextButton("Ingreso Hrs Extra", on_click=lambda _:clickHrsEx(page))
     btReporte = ft.TextButton("Reporte de horas por empleado", on_click=lambda _:clickReporte(page))
-    filatitulo = ft.row()
-    filabotones = ft.row()
-    filatitulo.controls = [titulo]
-    filabotones.controls = [btHORAS,btReporte]
-    page.add(filabotones, filatitulo)
+    btEmpleado = ft.TextButton("Ingreso de empleado", on_click=lambda _:clickEmpleado(page))
+    filaTitulo = ft.Row()
+    filaBotones = ft.Row()
+    filaTitulo.controls = [titulo]
+    filaBotones.controls = [btEmpleado,btHORAS,btReporte]
+    page.add(filaTitulo, filaBotones)
 
     def clickHrsEx(page:ft.Page):
       pass
 
     def clickReporte(page: ft.page):
        pass
-
+    def clickEmpleado(page: ft.page):
+       pass
 ft.app(target = VentanaPrincipal)
