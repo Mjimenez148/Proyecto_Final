7ea0414aaba272c9d055fb0f0fda3cb80958abf2
Mjimenez148 <mjimenezh5@miumg.edu.gt>
2024-05-21 15:27:53 -0600

    Update proyecto_final.py
    ya tenemos la funcion para agregar un empleado donde se colocan los datos de nombre, codigo de empleado y tabien el costo por hora extra

--- proyecto_final.py
+++ proyecto_final.py
@@ -6,6 +6,7 @@ def VentanaPrincipal(page:ft.Page):
     page.theme_mode = ft.ThemeMode.LIGHT
     page.update()
     titulo= ft.Text("Registro de Personal")
+    mensajes= ft.Text("Area de mensajes")
     codigo = ft.TextField(label = 'Codigo empleado', autofocus=True)
     nombre =ft.TextField(label='Nombre')
     costo_hr_extra = ft.TextField(label = ' Costo por Hora extra')
@@ -15,6 +16,8 @@ def VentanaPrincipal(page:ft.Page):
     btEmpleado = ft.TextButton("Ingreso de empleado", on_click=lambda _:clickEmpleado(page))
     filaTitulo = ft.Row()
     filaBotones = ft.Row()
+    colFormulario = ft.Column()
+    filaContenido = ft.Row()
     filaTitulo.controls = [titulo]
     filaBotones.controls = [btEmpleado,btHORAS,btReporte]
     page.add(filaTitulo, filaBotones)
@@ -25,5 +28,20 @@ def VentanaPrincipal(page:ft.Page):
     def clickReporte(page: ft.page):
        pass
     def clickEmpleado(page: ft.page):
+       filaBotones2 = ft.Row()
+       btnGuardar = ft.ElevatedButton("Guardar", on_click=lambda _:guardar(page))
+       btnCancelar = ft.ElevatedButton("Cancelar", on_click=lambda _:borrarCampos())
+       filaBotones2.controls = [btnGuardar, btnCancelar]
+       colFormulario.controls = [codigo, nombre, costo_hr_extra]
+       filaContenido.controls = [colFormulario]
+       mensajes.value='Ingreso de datos Nuevo Empleado'
+       borrarCampos()
+       page.update()
+       
+
+    def guardar(page: ft.page):
+       pass
+
+    def borrarCampos():
        pass
 ft.app(target = VentanaPrincipal)
